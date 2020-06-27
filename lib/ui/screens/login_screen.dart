92380ad37ea15d09c4b8608747c032136d2362c2
Daman <singhdaman4321@gmail.com>
2020-06-27 23:44:36 +0530

    Add bottom navigation bar

---
+++ lib/ui/screens/login_screen.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:saviour/ui/widgets/signin_button.dart';
+
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.white,
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              FlutterLogo(size: 150),
+              SizedBox(height: 50),
+              signInButton(context),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
