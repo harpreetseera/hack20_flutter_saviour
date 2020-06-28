bc84f33151a61ae548ff43d9eaad7a5096237a32
harpreetseera <harpreetseera8525@gmail.com>
2020-06-28 22:54:30 +0530

    add fcm and handle background notication

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:saviour/ui/screens/home_screen.dart';
@@ -8,7 +10,24 @@ import 'package:shared_preferences/shared_preferences.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   Saviour.prefs = await SharedPreferences.getInstance();
+  final FirebaseMessaging firebaseMessaging = FirebaseMessaging();
+  setupFirebase(firebaseMessaging);
   runApp(MyApp());
+}
+
+Future<dynamic> myBackgroundMessageHandler(Map<String, dynamic> message) {
+  if (message.containsKey('data')) {
+    // Handle data message
+    final dynamic data = message['data'];
+
+    print("background data - $data");
+  }
+
+  if (message.containsKey('notification')) {
+    // Handle notification message
+    final dynamic notification = message['notification'];
+    print("background notification - $notification");
+  }
 }
 
 class MyApp extends StatelessWidget {
@@ -29,6 +48,37 @@ class MyApp extends StatelessWidget {
       home: Saviour.prefs.getBool(Saviour.PREF_LOGGED_IN) ?? false
           ? HomeScreen()
           : LoginScreen(),
+    );
+  }
+}
+
+void setupFirebase(FirebaseMessaging firebaseMessaging) async {
+  String firebaseToken = await firebaseMessaging.getToken();
+  print('\nFirebase token : $firebaseToken\n');
+  if (defaultTargetPlatform == TargetPlatform.android) {
+    firebaseMessaging.configure(
+      onMessage: (Map<String, dynamic> message) async {
+        // BlocProvider.of<AppBloc>(context).add(
+        //   NotificationEvent(
+        //     message: message.toString(),
+        //   ),
+        // );
+
+        print("\nonMessage: $message");
+      },
+      onBackgroundMessage: myBackgroundMessageHandler,
+      onLaunch: (Map<String, dynamic> message) async {
+        await Future.delayed(Duration(seconds: 5));
+        // BlocProvider.of<AppBloc>(context)
+        //     .add(NotificationEvent(message: message.toString()));
+        print("\nonLaunch: $message");
+      },
+      onResume: (Map<String, dynamic> message) async {
+        await Future.delayed(Duration(seconds: 5));
+        // BlocProvider.of<AppBloc>(context)
+        //     .add(NotificationEvent(message: message.toString()));
+        print("\nonResume: $message");
+      },
     );
   }
 }
