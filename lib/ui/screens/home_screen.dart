72e94a02cf936dc3ccd224ae2c2fec66f092af46
harpreetseera <harpreetseera8525@gmail.com>
2020-06-28 06:53:10 +0530

    add google map with heat point integration

--- lib/ui/screens/home_screen.dart
+++ lib/ui/screens/home_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter_heatmap/google_maps_flutter_heatmap.dart';
 import 'package:saviour/ui/screens/issue_screen.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -7,6 +10,18 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  Completer<GoogleMapController> _controller = Completer();
+  final Set<Heatmap> _heatmaps = {};
+  static final CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+  LatLng _heatmapLocation = LatLng(37.42796133580664, -122.085749655962);
+  static final CameraPosition _kLake = CameraPosition(
+      bearing: 192.8334901395799,
+      target: LatLng(37.43296265331129, -122.08832357078792),
+      tilt: 59.440717697143555,
+      zoom: 19.151926040649414);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,6 +31,14 @@ class _HomeScreenState extends State<HomeScreen> {
               flex: 7,
               child: Container(
                 color: Colors.green,
+                child: GoogleMap(
+                  mapType: MapType.hybrid,
+                  initialCameraPosition: _kGooglePlex,
+                  heatmaps: _heatmaps,
+                  onMapCreated: (GoogleMapController controller) {
+                    _controller.complete(controller);
+                  },
+                ),
               )),
           Expanded(
               flex: 3,
@@ -25,15 +48,21 @@ class _HomeScreenState extends State<HomeScreen> {
                   child: Row(
                     children: <Widget>[
                       MaterialButton(
-                        onPressed: () {
-                          Navigator.of(context).push(MaterialPageRoute(
-                              builder: (context) => IssueScreen()));
-                        },
+                        onPressed: _goToTheLake,
+
+                        //  () {
+                        // Navigator.of(context).push(MaterialPageRoute(
+                        //     builder: (context) => IssueScreen()));
+                        // }
+                        // ,
                         color: Colors.red,
                         child: Text("Issues"),
                       ),
                       MaterialButton(
-                        onPressed: () {},
+                        onPressed:
+                            // () {
+                            _addHeatmap,
+                        // },
                         color: Colors.purple,
                         child: Text("Events"),
                       )
@@ -44,5 +73,36 @@ class _HomeScreenState extends State<HomeScreen> {
         ],
       ),
     );
+  }
+
+  void _addHeatmap() {
+    setState(() {
+      _heatmaps.add(Heatmap(
+          heatmapId: HeatmapId(_heatmapLocation.toString()),
+          points: _createPoints(_heatmapLocation),
+          radius: 20,
+          visible: true,
+          gradient: HeatmapGradient(
+              colors: <Color>[Colors.green, Colors.red],
+              startPoints: <double>[0.2, 0.8])));
+    });
+  }
+
+  List<WeightedLatLng> _createPoints(LatLng location) {
+    final List<WeightedLatLng> points = <WeightedLatLng>[];
+    //Can create multiple points here
+    points.add(_createWeightedLatLng(location.latitude, location.longitude, 1));
+    points.add(
+        _createWeightedLatLng(location.latitude - 1, location.longitude, 1));
+    return points;
+  }
+
+  WeightedLatLng _createWeightedLatLng(double lat, double lng, int weight) {
+    return WeightedLatLng(point: LatLng(lat, lng), intensity: weight);
+  }
+
+  Future<void> _goToTheLake() async {
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
   }
 }
